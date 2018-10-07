0581be0538851dbed315811b619a7cf8f2957191
Miroslav Stjepanovic <miroslav.stjepanovic7@yahoo.com>
2018-10-07 09:12:08 +0200

    Changed Comment and Post Feed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import profileReducer from "./profileReducer";
+import postReducer from "./postReducer";
 
 export default combineReducers({
   auth: authReducer, //this.props.auth kad koristimo authReducer u komponentama, ako hocemo da pozovemo action, ili uzemo propertije...
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
 
 //ovo je rootReducer
